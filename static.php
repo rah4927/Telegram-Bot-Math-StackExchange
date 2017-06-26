0447fe99b6fd11db637d4c6576fa79f4a9674b8d
rah4927 <rah4927@gmail.com>
2017-06-26 09:48:28 +0600

    Update static.php

--- static.php
+++ static.php
@@ -5,7 +5,7 @@ echo "Hello, world" ;
 $botToken = "330544559:AAEGpcl9x2qPAfS2tO9gEtmi2yT2AKkeyFQ" ;
 $website = "https://api.telegram.org/bot".$botToken ;
 
-$update = file_get_contents("php://input");
+$update = file_get_contents($website."\getupdates");
 
 
 $updateArray = json_decode($update, TRUE) ;
