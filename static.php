977b03ed3862ae7b2c5be43ab7683e41b5a2cf56
rah4927 <rah4927@gmail.com>
2017-06-26 08:37:07 +0600

    Update static.php

--- static.php
+++ static.php
@@ -1,4 +1,15 @@
-<?php echo "Hello, world" ; ?>
+<?php 
+
+
+echo "Hello, world" ;
+$botToken = "330544559:AAEGpcl9x2qPAfS2tO9gEtmi2yT2AKkeyFQ" ;
+$website = "https://api.telegram.org/bot".$botToken ;
+
+$update = file_get_contents($website."/getupdates") ;
+print_r($update) ;
+
+
+?>
 
 
 
