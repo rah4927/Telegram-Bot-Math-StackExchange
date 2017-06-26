93bf41fbb0aff03e2548d227c69c369c3173ccf5
rah4927 <rah4927@gmail.com>
2017-06-26 09:13:19 +0600

    Update static.php

--- static.php
+++ static.php
@@ -12,9 +12,12 @@ $updateArray = json_decode($update, TRUE) ;
 
 
 $chatId = $updateArray["result"][0]["message"]["chat"]["id"] ;
+$text = $updateArray["result"][0]["message"]["text"] ;
 
-file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test") ;
-
+if($text === "/hi")
+{
+  file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test") ;
+}
 ?>
 
 
