6e6104e5477997cad42f35af34c05c3945b61a94
rah4927 <rah4927@gmail.com>
2017-06-26 09:36:34 +0600

    Update static.php

--- static.php
+++ static.php
@@ -6,10 +6,10 @@ $botToken = "330544559:AAEGpcl9x2qPAfS2tO9gEtmi2yT2AKkeyFQ" ;
 $website = "https://api.telegram.org/bot".$botToken ;
 
 $update = file_get_contents("php://input");
-print_r($update) ;
+
 
 $updateArray = json_decode($update, TRUE) ;
-
+print_r($updateArray) ;
 
 $chatId = $updateArray["result"][0]["message"]["chat"]["id"] ;
 
